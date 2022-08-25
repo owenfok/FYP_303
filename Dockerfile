e3b593ef38506c54f9d13a583816452342ed10b8
Owen <owenfok119@gmail.com>
2022-08-25 13:04:03 +0800

    change port

--- Dockerfile
+++ Dockerfile
@@ -15,5 +15,5 @@ RUN npm install
 # Bundle app source
 COPY . .
 
-EXPOSE 8081
+EXPOSE 80
 CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.use(function (err, req, res, next) {
 });
 
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, function () {
   console.log('Server is started on http://127.0.0.1:'+PORT);
 });
